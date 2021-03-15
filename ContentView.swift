34b8df84b3f7aa8ccc1f2e69722fb5c7d3463d11
Richard Price <mrspondooli@gmail.com>
2021-03-15 13:25:25 +0000

    added form with published and state properties, place order button

--- ContentView.swift
+++ ContentView.swift
@@ -11,27 +11,73 @@ import SwiftUI
 class Order: ObservableObject {
     var didChange = PassthroughSubject<Void, Never>()
     static let types = ["Vanilla", "Choclate", "Strawberry", "Toffee"]
-    var type = 0
+    
+    @Published var type = 0 { didSet { update() } }
+    @Published var quantity = 3 { didSet { update() } }
+    @Published var extraFosting = false { didSet { update() } }
+    @Published var addSprinkles = false { didSet { update() } }
+    @Published var specialRequestsEnabled = false { didSet { update() } }
+    
+    @State var name = "" { didSet { update() } }
+    @State var streetAddress = "" { didSet { update() } }
+    @State var city = "" { didSet { update() } }
+    @State var zip = "" { didSet { update() } }
+    
     func update() {
         didChange.send()
     }
 }
 
 struct ContentView: View {
-    
     @ObservedObject var order = Order()
-    
     var body: some View {
         NavigationView {
             Form {
-                Picker(selection: $order.type, label: Text("select your cake type")) {
-                    ForEach(0 ..< Order.types.count) {
-                        Text(Order.types[$0]).tag($0)
+                Section {
+                    Picker(selection: $order.type, label: Text("select your cake type")) {
+                        ForEach(0 ..< Order.types.count) {
+                            Text(Order.types[$0]).tag($0)
+                        }
+                    }
+                    Stepper(value: $order.quantity, in: 3...20) {
+                        Text("Number of cakes: \(order.quantity)")
+                    }
+                }
+                Section {
+                    Toggle(isOn: $order.specialRequestsEnabled, label: {
+                        Text("Any Special Requests")
+                    })
+                    if order.specialRequestsEnabled {
+                        Toggle(isOn: $order.extraFosting, label: {
+                            Text("Add Extra Frosting")
+                        })
+                        Toggle(isOn: $order.addSprinkles, label: {
+                            Text("Add Sprinkles")
+                        })
+                    }
+                }
+                
+                Section {
+                    TextField("Name", text: order.$name)
+                    TextField("Street Address", text: order.$streetAddress)
+                    TextField("City", text: order.$city)
+                    TextField("Zip", text: order.$zip)
+                }
+                
+                Section {
+                    Button(action: {
+                        self.placeOrder()
+                    }) {
+                        Text("Place Order")
                     }
                 }
             }
             .navigationBarTitle("Cupcake World")
         }
+    }
+    
+    func placeOrder() {
+        
     }
 }
 
